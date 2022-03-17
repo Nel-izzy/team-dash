c4b67d6462c896606f4e8d38462f17d12ebe6b5d
nel-izzy <nelsoninho11@gmail.com>
2022-03-17 15:55:35 +0100

    edited sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,50 +5,50 @@ const Sidebar = () => {
     <div className="sidebar-container">
       <div className="sidebar-header">
         <div>
-          <i class="bi bi-stack"></i>
+          <i className="bi bi-stack"></i>
         </div>
-
+ 
         <div>
           <h3 className="sidebar-header-h3">PJ</h3>
         </div>
         <div>
-          <i class="bi bi-arrow-bar-left"></i>
+          <i className="bi bi-arrow-bar-left"></i>
         </div>
       </div>
       <ul className="sidebar-ul">
         <li className="sidebar-li">
           <i className="bi bi-ui-checks-grid"></i>
-          <a href="#" className="sidebar-a">
+          <a href="/" className="sidebar-a">
             Dashboard
           </a>
         </li>
         <li className="sidebar-li">
           <i class="bi bi-bar-chart"></i>
-          <a href="" className="sidebar-a">
+          <a href="/" className="sidebar-a">
             Analytics
           </a>
         </li>
         <li className="sidebar-li">
           <i class="bi bi-folder-fill"></i>
-          <a href="" className="sidebar-a">
+          <a href="/" className="sidebar-a">
             Projects
           </a>
         </li>
         <li className="sidebar-li">
           <i class="bi bi-stopwatch-fill"></i>
-          <a href="" className="sidebar-a">
+          <a href="/" className="sidebar-a">
             Tracking
           </a>
         </li>
         <li className="sidebar-li">
           <i class="bi bi-clock-history"></i>
-          <a href="" className="sidebar-a">
+          <a href="/" className="sidebar-a">
             History
           </a>
         </li>
         <li className="sidebar-li">
           <i className="bi bi-gear-fill"></i>
-          <a href="" className="sidebar-a">
+          <a href="/" className="sidebar-a">
             Settings
           </a>
         </li>
